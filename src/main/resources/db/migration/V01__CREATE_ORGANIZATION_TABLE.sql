388ca41ed3202fad59f0ce0a267c8daa57a59389
Bruno Dantas <brunoodantas7@gmail.com>
2021-10-13 23:19:01 -0300

    Refactor flyway migrations to one file , in case of uses flyway the migrations init default data base data at API first use.

--- src/main/resources/db/migration/V01__CREATE_ORGANIZATION_TABLE.sql
+++ src/main/resources/db/migration/V01__CREATE_ORGANIZATION_TABLE.sql
@@ -2,7 +2,7 @@
 create table if not exists organizations
 (
     id bigserial
-        constraint organizations_pk
+        constraint organizations_pkey
             primary key,
     register_number varchar,
     corporate_name varchar
@@ -10,4 +10,37 @@ create table if not exists organizations
 );
 
 insert into organizations(corporate_name,register_number) values ('Amazing Company',0981234567);
+-------------------------------------------------------------
+
+create table if not exists users
+(
+    id bigserial
+        constraint users_pkey
+            primary key,
+    login varchar,
+    password text,
+    organization_id bigint not null
+        constraint fk_organizations_users
+            references organizations
+);
+
+insert into users(login,password,organization_id) values ('admin',123,1);
+
+-------------------------------------------------------------
+
+create table if not exists departments
+(
+    id              bigserial
+        constraint departments_pkey
+            primary key,
+    description     varchar(255),
+    organization_id bigint not null
+        constraint fk_organizations_departments
+            references organizations
+);
+
+insert into departments (description, organization_id)  values ('Finances', 1);
+
+
+
 
