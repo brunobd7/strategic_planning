485be3ecdbb5f6f731c2dc8e5e065672250c26b0
Bruno Dantas <brunoodantas7@gmail.com>
2022-07-18 23:58:35 -0300

    Mapping interfaces of BSC method

---
+++ src/main/java/com/dantas/strategicplanning/repository/BscObjectiveRepository.java
@@ -0,0 +1,7 @@
+package com.dantas.strategicplanning.repository;
+
+import com.dantas.strategicplanning.model.BscObjective;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BscObjectiveRepository extends JpaRepository<Long, BscObjective> {
+}
---
+++ src/main/java/com/dantas/strategicplanning/repository/BscPerspectiveRepository.java
@@ -0,0 +1,7 @@
+package com.dantas.strategicplanning.repository;
+
+import com.dantas.strategicplanning.model.BscPerspective;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BscPerspectiveRepository extends JpaRepository<Long, BscPerspective> {
+}
